80b75dc555204de54678098889de78b8503cf189
Anton <av@dsx.uk>
2019-05-29 02:03:20 +0300

    small style fixes

--- src/components/SideBar/sidebar.jsx
+++ src/components/SideBar/sidebar.jsx
@@ -5,13 +5,10 @@ import SidebarNav from '../SidebarNav/SidebarNav.jsx'
 import imageplusv1 from "../../assets/img/plusv1.png";
 
 
-
 export default class Sidebar extends React.PureComponent {
     constructor() {
         super();
-        this.state = {
-
-        }
+        this.state = {}
     }
 
     render() {
@@ -21,44 +18,13 @@ export default class Sidebar extends React.PureComponent {
                 <div className="sidebar__name"><img className="logo" src={image}/>BLANK!</div>
                 {
                     this.props.universeList.map((universe, i) => {
-                        return(
+                        return (
                             <SidebarNav isOpen="true"
                                         key={i}
                                         data={universe}/>
                         )
                     })
                 }
-
-
-                {/*<ul className="navbar-nav mr-auto">*/}
-                {/*    <li className="nav-item active">*/}
-                {/*        <Link to={'/'} className="nav-link">Start Page</Link>*/}
-                {/*    </li>*/}
-                {/*    <li className="nav-item">*/}
-                {/*        <Link to={'/createHero'} className="nav-link">CreateHero</Link>*/}
-                {/*    </li>*/}
-                {/*    <li className="nav-item">*/}
-                {/*        <Link to={'/createUniverse'} className="nav-link">CreateUniverse</Link>*/}
-                {/*    </li>*/}
-                {/*    <li className="nav-item">*/}
-                {/*        <Link to={'/createLocation'} className="nav-link">CreateLoc</Link>*/}
-                {/*    </li>*/}
-                {/*    <li className="nav-item">*/}
-                {/*        <Link to={'/heroList'} className="nav-link">Top CreationsHero<span*/}
-                {/*            className="sr-only">(current)</span></Link>*/}
-                {/*    </li>*/}
-                {/*    <li className="nav-item">*/}
-                {/*        <Link to={'/chooseCreation'} className="nav-link">ChooseCreation<span*/}
-                {/*            className="sr-only">(current)</span></Link>*/}
-                {/*    </li>*/}
-                {/*    <li className="nav-item">*/}
-                {/*        <Link to={'/LocationList'} className="nav-link">Top CreationsLoc<span*/}
-                {/*            className="sr-only">(current)</span></Link>*/}
-                {/*    </li>*/}
-                {/*    <li className="nav-item">*/}
-                {/*        <Link to={'/myBlank'} className="nav-link">My Blank</Link>*/}
-                {/*    </li>*/}
-                {/*</ul>*/}
 
                 <Link to={'/createUniverse'}><img className="sidebar_plusv1" src={imageplusv1}></img></Link>
             </div>
