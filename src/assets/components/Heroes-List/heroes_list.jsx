8e27bbcc2262e0028ab2377ce778b2697db15232
Formzet <leva200206@gmail.comgit config --global user.email leva200206@gmail.comgit config --global user.email leva200206@gmail.com>
2019-05-28 15:47:20 +0300

    sec

--- src/assets/components/Heroes-List/heroes_list.jsx
+++ src/assets/components/Heroes-List/heroes_list.jsx
@@ -15,12 +15,13 @@ export default class HeroList extends React.Component {
     render() {
         let heroes = this.props.heroes.map((hero, i) => {
             return(
-                <div className="col-md-3" key={i}>
+                <div className="col-md-2" key={i}>
                     <div className="card">
 
                         <div className="card-body ">
 
                             <img src={hero.img}/>
+
                             <h2>{hero.name}</h2>
                             <h3>{hero.size}</h3>
 
