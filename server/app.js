6b750b53fa868f9f8b4caf37a3e59cb5071e3bfd
Anton <av@dsx.uk>
2019-05-29 01:59:45 +0300

    add server folder

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+const MONGODB = "mongodb://localhost/blank_universe";
+
+mongoose.connect(MONGODB, {
+    useNewUrlParser: true
+});
+require('./models/UniverseModel');
+require('./models/HeroModel');
+require('./models/LocationModel');
+require('./routes/blank_route')(app);
+
+const PORT = process.env.PORT || 3005;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Here we go on PORT ${PORT}`)
+});
+
+
